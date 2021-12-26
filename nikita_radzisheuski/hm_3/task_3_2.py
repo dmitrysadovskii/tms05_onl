9f874434007da7a04eed5ba01a1086c40d9a104c
Nikita Radzisheuski <nickitque.39@gmail.com>
2021-12-26 15:35:05 +0300

    Adding Homework #3 files

---
+++ nikita_radzisheuski/hm_3/task_3_2.py
@@ -0,0 +1,10 @@
+
+"""Adding ing to words. In my case I created string with text ->
+splitted words -> added ing -> transformed it back to str"""
+
+string_with_words = 'This is just string to do homework number three and to add ing'
+splitting_the_words = string_with_words.split()
+splitting_the_words = [words + "ing" for words in splitting_the_words]
+list_to_string = ' '.join([str(elem) for elem in splitting_the_words])
+
+print(list_to_string)
