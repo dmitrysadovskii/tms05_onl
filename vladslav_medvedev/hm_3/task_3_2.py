3c426cae3452f311313e9d4304cf5a8d5b45b862
Vladislav Medvedev <vladm201098112@gmail.com>
2021-12-26 20:16:47 +0100

    First commit

---
+++ vladslav_medvedev/hm_3/task_3_2.py
@@ -0,0 +1,6 @@
+# Напишите программу, которая добавляет ‘ing’ к словам.
+word = ["salt", "dog", "bootle", 'eat', 'open']
+new_word = []
+for k in word:
+    new_word.append(k + 'ing')
+print(new_word)
