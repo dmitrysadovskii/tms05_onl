211bfd4f6389b330674f361d46a585607344d67a
Vlad Maksimenko <2dayday39@gmail.com>
2021-12-26 19:58:43 +0300

    task2 done

---
+++ vlad_maksimenko/hm_3/task_3_2.py
@@ -0,0 +1,14 @@
+"""first"""
+new = "Push"
+ing = "ing"
+print(new + ing)
+
+"""second"""
+new = "Push"
+ing = "ing"
+
+print("{word}".format(word = new + ing))
+
+"""third"""
+new = "Push"
+print(f"{new+ing}")
