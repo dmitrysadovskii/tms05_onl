c69257a732c5166cb4170cca830f2004de53ba0f
Vlad Maksimenko <2dayday39@gmail.com>
2021-12-27 17:38:46 +0300

    task2 done

--- vlad_maksimenko/hm_3/task_3_2.py
+++ vlad_maksimenko/hm_3/task_3_2.py
@@ -1,13 +1,4 @@
-"""first"""
-new = "Push"
-ing = "ing"
-print(new + ing)
-
-"""second"""
-new = "Push"
-ing = "ing"
-print("{word}".format(word = new + ing))
-
-"""third"""
-new = "Push"
-print(f"{new+ing}")
+word = ['red', 'green', 'blue']
+c = "ing"
+b = 'ing, '.join(word)+c
+print(b)
