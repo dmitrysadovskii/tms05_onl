790258be66a85d80ef17bbb367e2fcd451cdfee8
AnastasiyaMava <matyshava@gmail.com>
2021-12-29 22:16:24 +0300

    Ivanou Ivan

--- anastasiya_matysheva/hm_3/task_3_3.py
+++ anastasiya_matysheva/hm_3/task_3_3.py
@@ -1,4 +1,4 @@
-p = input('Ivanov Ivan')
-name = p[:p.find('Ivan')]
-surname = p[p.find('Ivanov') + 1:]
-print(surname + ' ' + name)
+N = 'Ivanou Ivan'
+name = ' '.join(N.split(' ')[::-1])
+print(name)
+
