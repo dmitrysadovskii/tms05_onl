f21384f645818ec11706fe7ebd0842cceb9f2c44
Vadim Shmelev <smelevv88@gmail.com>
2021-12-30 13:49:12 +0300

    replaced / to #

---
+++ vadim_shmelev/hm3/task_3_2.py
@@ -0,0 +1,6 @@
+word = ['wait', 'meat']
+word_new = []
+for element in word:
+    word_new.append(element + 'ing')
+print(word_new)
+
---
+++ vadim_shmelev/hm3/task_3_4.py
@@ -0,0 +1,4 @@
+test_string = '  Hellp Dzmitry!  '
+new_string = test_string.split()
+print(' '.join(new_string))
+
