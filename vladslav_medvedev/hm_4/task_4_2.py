387b561de21900159f7259a041d7ebf5949a8031
Vladislav Medvedev <vladm201098112@gmail.com>
2022-01-03 23:16:52 +0100

    First commit

--- vladslav_medvedev/hm_4/task_4_2.py
+++ vladslav_medvedev/hm_4/task_4_2.py
@@ -1,5 +1,7 @@
-#Дан список: [‘Ivan’, ‘Ivanou’], и 2 строки: Minsk, Belarus
-#Напечатайте текст: “Привет, Ivan Ivanou! Добро пожаловать в Minsk Belarus”
+"""
+Дан список: [‘Ivan’, ‘Ivanou’], и 2 строки: Minsk, Belarus
+Напечатайте текст: “Привет, Ivan Ivanou! Добро пожаловать в Minsk Belarus”
+"""
 name_sur = ['Ivan', 'Ivanou']
 loc_c = str("Minsk")
 loc_city = 'Belatus'
