94eb0958c364f662336e93e93aecc63374752b05
Veronika Ivashko <veronika.ivashko25279@gmail.com>
2022-01-07 01:24:50 +0300

    added files for homework 4

---
+++ veronika_ivashko/hw_4/task_4_2.py
@@ -0,0 +1,11 @@
+"""
+2. Дан список: [‘Ivan’, ‘Ivanou’], и 2 строки: Minsk, Belarus
+Напечатайте текст: “Привет, Ivan Ivanou! Добро пожаловать в Minsk Belarus”
+"""
+
+first_last_name = ['Ivan', 'Ivanou']
+city = 'Minsk'
+country = 'Belarus'
+
+print(f'Привет, {first_last_name[0]} {first_last_name[1]}! '
+      f'Добро пожаловать в {city} {country}')
