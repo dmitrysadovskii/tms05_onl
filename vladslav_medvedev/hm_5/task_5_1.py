fcf36085a0717c08d126115815908b77587bc9e7
Vladislav Medvedev <vladm201098112@gmail.com>
2022-01-09 19:43:59 +0100

    First commit

--- vladslav_medvedev/hm_5/task_5_1.py
+++ vladslav_medvedev/hm_5/task_5_1.py
@@ -8,13 +8,18 @@
 const = '3219'
 for i in range(100):
     n, count_c, count_b = 0, 0, 0
-    input_1 = ''
-    input_1 = input('Введите 4х значное число: ')
+    user_input = input('Введите 4х значное число: ')
+    count_user_input = len(user_input)
+    for i in range(1):
+        if count_user_input > 0 or count_user_input < 4\
+                or count_user_input != '' or not count_user_input.isdigit():
+            print('Значение введено неверно! Повторите повытку!')
+    break
     for i in const:
-        if i == input_1[n]:
+        if i == user_input[n]:
             count_c += 1
             n += 1
-        elif i != input_1[n]:
+        elif i != user_input[n]:
             count_b += 1
             n += 1
     if count_c > 3:
@@ -23,7 +28,6 @@ for i in range(100):
     else:
         print(f'Кол-во Коров:{count_c}')
         print(f'Кол-во Быков:{count_b}')
-
 '''
 Правила немного изменились
 пс на написание этой игры ушло много часов
