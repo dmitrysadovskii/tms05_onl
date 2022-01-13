4df85205c11b1180b1d3cc048e2cabd637540fa3
Vadim Shmelev <smelevv88@gmail.com>
2022-01-13 22:57:02 +0300

    fix circle ci errors

--- vadim_shmelev/hm6/task_6_1.py
+++ vadim_shmelev/hm6/task_6_1.py
@@ -1,11 +1,14 @@
 from functools import reduce
 
+
 def luhn(card_numb):
+
     numbers = (0, 2, 4, 6, 8, 1, 3, 5, 7, 9)
     card_numb = reduce(str.__add__, filter(str.isdigit, card_numb))
     evens = sum(int(i) for i in card_numb[-1::-2])
     odds = sum(numbers[int(i)] for i in card_numb[-2::-2])
     return (evens + odds) % 10 == 0
+
 
 card_numb = input('Enter card number:')
 while True:
