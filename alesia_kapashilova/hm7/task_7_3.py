395d1632b9a5fb1b73003a606420ae6fbec48ce8
Alesia Kapashilova <alesia.andreevna@gmail.com>
2022-01-17 17:15:59 +0300

    fixed CI

--- alesia_kapashilova/hm7/task_7_3.py
+++ alesia_kapashilova/hm7/task_7_3.py
@@ -9,42 +9,42 @@ lan = input()
 print("Выберите шифрование: шифрование - ch, дешифрование - def")
 chif = input()
 print("Введите ключ шифрования")
-n = int(input())
+key = int(input())
 print("Введите фразу")
-f = input()
+phrase = input()
 
 
-def chru(chifr, n, l, fraza):
-    if l == 'r':
+def chru(chifr, k, lang, fraza):
+    if lang == 'r':
         moch = 32
-    if l == 'e':
+    if lang == 'e':
         moch = 26
     if chifr == 'def':
-        n = -n
+        k = -k
     for i in range(len(fraza)):
         if fraza[i].isalpha():
             if fraza[i] == fraza[i].upper():
                 for j in range(moch):
                     if moch == 32:
                         if fraza[i] == rus_upper_alphabet[j]:
-                            print(rus_upper_alphabet[(j + n) % moch], end='')
+                            print(rus_upper_alphabet[(j + k) % moch], end='')
                             break
                     if moch == 26:
                         if fraza[i] == eng_upper_alphabet[j]:
-                            print(eng_upper_alphabet[(j + n) % moch], end='')
+                            print(eng_upper_alphabet[(j + k) % moch], end='')
                             break
             elif fraza[i] == fraza[i].lower():
                 for j in range(moch):
                     if moch == 32:
                         if fraza[i] == rus_lower_alphabet[j]:
-                            print(rus_lower_alphabet[(j + n) % moch], end='')
+                            print(rus_lower_alphabet[(j + k) % moch], end='')
                             break
                     if moch == 26:
                         if fraza[i] == eng_lower_alphabet[j]:
-                            print(eng_lower_alphabet[(j + n) % moch], end='')
+                            print(eng_lower_alphabet[(j + k) % moch], end='')
                             break
         else:
             print(fraza[i], end='')
 
 
-chru(chif, n, lan, f)
+chru(chif, key, lan, phrase)
