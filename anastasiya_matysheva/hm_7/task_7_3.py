b9e6ff12036b8152d5474ed4a2037544334efd5e
AnastasiyaMava <matyshava@gmail.com>
2022-01-17 22:48:01 +0300

    Cezar code

--- anastasiya_matysheva/hm_7/task_7_3.py
+++ anastasiya_matysheva/hm_7/task_7_3.py
@@ -1,27 +1,32 @@
-def cezar_code(string, number, dec, lang ):
+def cezar_code(string, number, dec, lang):
     alpha_en = 'abcdefghijklmnopqrstuvwxyz'
     alpha_ru = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
     result = ''
     if dec == 'enc':
         if lang == 'ru':
             for c in string:
-                result += alpha_ru[(alpha_ru.index(c) + number) % len(alpha_ru)]
+                result += alpha_ru[(alpha_ru.index(c)
+                                    + number) % len(alpha_ru)]
         elif lang == 'en':
             for c in string:
-                result += alpha_en[(alpha_en.index(c) + number) % len(alpha_en)]
+                result += alpha_en[(alpha_en.index(c)
+                                    + number) % len(alpha_en)]
     else:
         if lang == 'ru':
             for c in string:
-                result += alpha_ru[(alpha_ru.index(c) - number) % len(alpha_ru)]
+                result += alpha_ru[(alpha_ru.index(c)
+                                    - number) % len(alpha_ru)]
         elif lang == 'en':
             for c in string:
-                result += alpha_en[(alpha_en.index(c) - number) % len(alpha_en)]
+                result += alpha_en[(alpha_en.index(c)
+                                    - number) % len(alpha_en)]
     return result
 
 
 print(cezar_code(input('Enter world: '), int(input('Enter the step: ')),
                  input('Choose decode or encode (enter "dec" or "enc"): '),
                  input('Choose english or russian (enter "en" or "ru"); ')))
+
 
 
 
