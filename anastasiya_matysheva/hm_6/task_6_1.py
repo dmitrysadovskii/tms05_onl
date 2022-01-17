96831b22e674176c0377850ca8cbf939c6c28776
AnastasiyaMava <matyshava@gmail.com>
2022-01-17 12:26:01 +0300

    Fixed search credit card

--- anastasiya_matysheva/hm_6/task_6_1.py
+++ anastasiya_matysheva/hm_6/task_6_1.py
@@ -4,25 +4,28 @@ import re
 
 def luhn(code):
     look_up = (0, 2, 4, 6, 8, 1, 3, 5, 7, 9)
-    code = reduce(str.__add__, filter(str.isdigit, code))
+    code = reduce(str.__add__,filter(str.isdigit, code))
     evens = sum(int(i) for i in code[-1::-2])
     odds = sum(look_up[int(i)] for i in code[-2::-2])
     return (evens + odds) % 10 == 0
 
 
-card_numb = input('Please, enter credit card number: ')
-opt = re.compile("[ -\\/:-@\\[-\\`{-~ \t]")
+def check_card():
+    card_numb = input('Please, enter credit card number: ')
+    opt = re.compile("[ -\\/:-@\\[-\\`{-~ \t]")
 
-if opt.search(card_numb):
-    print('Please, enter credit card number without space or special simbol')
-elif card_numb.islower():
-    print('Please, enter only numbers')
-elif len(card_numb) > 16:
-    print('Please, enter no more than 16 numbers')
-elif not card_numb:
-    print('Please, enter credit card number')
-else:
-    if luhn(card_numb):
-        print('Credit card is valid')
+    if opt.search(card_numb):
+        print('Please, enter credit card number without space or special simbol')
+    elif card_numb.islower():
+        print('Please, enter only numbers')
+    elif len(card_numb) > 16:
+        print('Please, enter no more than 16 numbers')
+    elif not card_numb:
+        print('Please, enter credit card number')
     else:
-        print('Credit card invalid')
+        if luhn(card_numb):
+            print('Credit card is valid')
+        else:
+            print('Credit card invalid')
+
+check_card()
