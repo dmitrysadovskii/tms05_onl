d726044f24ecd0e23c04f46cc5abe1b3084f0de8
Valeria Agrest <agrestval@gmail.com>
2022-01-23 22:14:21 +0300

    added 2 tasks in homework9

---
+++ valeria_agrest/hm_9/task_9_1.py
@@ -0,0 +1,59 @@
+class Book:
+
+    def __init__(self, book, author, pages, isbn, reserved, taken):
+        self.book = book
+        self.author = author
+        self.pages = pages
+        self.isbn = isbn
+        self.reserved = reserved
+        self.taken = taken
+
+
+class User:
+
+    def __init__(self):
+        self.books = list()
+
+    def book_to_take(self, a_book):
+        self.books.append(a_book)
+
+    def take_book(self):
+        for a_book in self.books:
+            if a_book.taken == 0 and a_book.reserved == 0:
+                print(f'You have taken {a_book.book}.')
+                a_book.taken += 1
+            elif a_book.taken == 0 and a_book.reserved != 0:
+                print(f'{a_book.book} has already reserved. '
+                      f'Please choose another book to take.')
+            elif a_book.taken != 0:
+                print(f'{a_book.book} has already taken. '
+                      f'Please choose another book to take.')
+
+    def reserve_book(self):
+        for a_book in self.books:
+            if a_book.taken == 0 and a_book.reserved == 0:
+                print(f'You have reserved {a_book.book}.')
+                a_book.reserved += 1
+            elif a_book.taken != 0 and a_book.reserved == 0:
+                print(f'{a_book.book} has already taken. '
+                      f'Please choose another book to reserve.')
+            elif a_book.reserved != 0:
+                print(f'{a_book.book} has already reserved. '
+                      f'Please choose another book to reserve.')
+
+    def bring_back_book(self):
+        for a_book in self.books:
+            if a_book.taken == 1:
+                a_book.taken = 0
+                print(f'Thank you for bringing back {a_book.book}')
+
+
+book = Book('A Walk to Remember', 'Nicholas Sparks', 240, 0-446-60895-5, 0, 0)
+User1 = User()
+User1.book_to_take(book)
+User1.take_book()
+User2 = User()
+User2.book_to_take(book)
+User2.reserve_book()
+User1.bring_back_book()
+User2.reserve_book()
---
+++ valeria_agrest/hm_9/task_9_2.py
@@ -0,0 +1,31 @@
+class Investment:
+
+    def __init__(self, summa, period, percent):
+        self.summa = summa
+        self.period = period
+        self.percent = percent
+
+
+class Bank:
+
+    def __init__(self):
+        self.invests = list()
+
+    def add_invest(self, invest):
+        self.invests.append(invest)
+
+    def deposit(self):
+        for invest in self.invests:
+            period_in_months = invest.period * 12
+            percent_in_one_month = invest.percent / 12
+            for i in range(1, period_in_months + 1):
+                new_summa = invest.summa + \
+                            (invest.summa * percent_in_one_month) / 100
+                invest.summa = round(new_summa, 2)
+            print(f'Summa will be {invest.summa}')
+
+
+investment = Investment(2000, 1, 10)
+bank = Bank()
+bank.add_invest(investment)
+bank.deposit()
