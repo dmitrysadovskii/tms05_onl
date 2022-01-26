e229634c327733923e47da5d2c56cd2deb8c9454
Nikita Radzisheuski <nickitque.39@gmail.com>
2022-01-26 23:17:24 +0300

    Editing Class Book

--- nikita_radzisheuski/hm_9/task_9_1.py
+++ nikita_radzisheuski/hm_9/task_9_1.py
@@ -10,15 +10,26 @@ class Book:
     """Class attribute for counting books"""
     books_count = 0
     """Creating class methods"""
-    def add_new_book(self, book_name, author, pages_quantity, isbn, flag, reserved):
+    def __init__(self, book_name, author, pages_quantity, isbn, flag, reserved):
         self.book_name = book_name
         self.author = author
         self.pages_quantity = pages_quantity
         self.isbn = isbn
         self.flag = flag
         self.reserved = reserved
+        Book.books_count += 1
 
+book_1 = Book("Neznayka", "Somebody", 505, 978-3-16-148410-0, "BY", False)
+book_2 = Book("A Byte of Python", "Swaroop C H", 164, 978-5-16-122410-0, "US", False)
+book_3 = Book("Neznayka2", "Somebody", 505, 978-3-16-148410-0, "BY", False)
+#print(book_1)
+print(Book.books_count)
 
 class User:
     def get_book(self):
         pass
+    def return_book(self):
+        pass
+    def book_a_book(self):
+        pass
+
