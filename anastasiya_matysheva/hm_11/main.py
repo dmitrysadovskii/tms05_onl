4d88562eab8ae1bcd8e0cf779cf7f49ed1ae2693
AnastasiyaMava <matyshava@gmail.com>
2022-02-01 10:11:11 +0300

    Calculator

---
+++ anastasiya_matysheva/hm_11/main.py
@@ -0,0 +1,60 @@
+import func
+
+
+def validation(x, y):
+    try:
+        x = float(x)
+        y = float(y)
+    except ValueError:
+        raise TypeError("Invalid data type")
+
+
+class Calculator(func.Methods):
+
+    @staticmethod
+    def sum(x, y):
+        try:
+            validation(x, y)
+            result = float(x) + float(y)
+            print(f"The result is {result}")
+        except Exception as error:
+            print(error)
+
+    @staticmethod
+    def deduction(x, y):
+        try:
+            validation(x, y)
+            result = float(x) - float(y)
+            print(f"The result is {result}")
+        except Exception as error:
+            print(error)
+
+    @staticmethod
+    def multiplication(x, y):
+        try:
+            validation(x, y)
+            result = float(x) * float(y)
+            print(f"The result is {result}")
+        except Exception as error:
+            print(error)
+
+    @staticmethod
+    def division(x, y):
+        try:
+            validation(x, y)
+            if float(y) == 0:
+                raise ZeroDivisionError("Division by 0!")
+            else:
+                result = float(x) / float(y)
+                print(f"The result is {result}")
+        except Exception as error:
+            print(error)
+
+
+x = input("Enter value 1: ")
+y = input("Enter value 2: ")
+calculator = Calculator()
+calculator.sum(x, y)
+calculator.deduction(x, y)
+calculator.multiplication(x, y)
+calculator.division(x, y)
