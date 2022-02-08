b3faac05f04e651db229b3879e366cf649d3e884
Alesia Kapashilova <alesia.andreevna@gmail.com>
2022-02-08 18:51:23 +0300

    fixed remarks

--- alesia_kapashilova/hm7/task_7_1.py
+++ alesia_kapashilova/hm7/task_7_1.py
@@ -2,13 +2,16 @@ def reshape(first_array: list, num_rows: int, num_col: int) -> list:
     num_len = len(first_array)
     actual_len = num_col * num_rows
     if actual_len > num_len:
-        first_array.extend([0 for _ in range(actual_len - num_len)])
-
-    k = 0
+        print('Длина списка не соответствует введенным значениям!!!')
+        return []
     new_array = []
-    for i in range(num_col, actual_len + 1, num_col):
-        new_array.append(first_array[k:i])
-        k = i
+    for i in range(num_rows):
+        new_array.append([first_array[k] for k in
+                          range(num_col * i, num_col * i + num_col)])
+
+    if actual_len != len(first_array):
+        new_array.append(first_array[actual_len:])
+
     return new_array
 
 
