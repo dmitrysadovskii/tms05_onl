4c919d4c408178c77cf81ee9f33ac59b602e2619
Vladislav Medvedev <vladm201098112@gmail.com>
2022-02-13 16:37:38 +0100

    1 commit

--- vladslav_medvedev/hm_10/task_10_1.py
+++ vladslav_medvedev/hm_10/task_10_1.py
@@ -31,10 +31,10 @@ class Roses(Flowers):
         self.price = 25
 
 
-class Charomile(Flowers):
+class Chamomile(Flowers):
 
     def __init__(self):
-        self.name = 'Charomile'
+        self.name = 'Chamomile'
         self.country = 'Russia'
         self.color = "white"
         self.long = 20
@@ -64,53 +64,35 @@ class Peony(Flowers):
 
 class Bouquet:
 
-    def __init__(self, fl, counts):
-        self.fl = fl
-        self.counts = counts
+    def __init__(self,):
+        count_roses_in_the_bouquet = range(0, 5)
+        count_chamomile_in_the_bouquet = range(0, 5)
+        count_iris_in_the_bouquet = range(0, 5)
+        count_peon_in_the_bouquet = range(0, 5)
+        count_flowers_in_the_bouquet = count_roses_in_the_bouquet + count_chamomile_in_the_bouquet + count_iris_in_the_bouquet + count_peon_in_the_bouquet
 
-    def price_of_bouquet(self):
-        price_list = []
-        for flower in self.fl:
-            each_price = self.counts[self.fl.index(flower)] * flower.price
-            price_list.append(each_price)
-        new_price_list = sum(price_list)
-        print(f"The price of the bouquet is {new_price_list}")
-
-    def withering_time(self):
-        lifetime = []
-        for flower in self.fl:
-            lifetime.append(flower.life_time)
-        lifetime = round(sum(lifetime) / len(self.fl), 1)
-        print(f"The withering time of the bouquet is {lifetime} days")
-
-    def sort_option(self, option):
-        assert isinstance(option, str), "Option should be str"
-        assert len(option) != 0, "Please enter an option"
-        assert option in ['life_time', 'price', 'length', 'color', 'fresh'], \
-            f"There is no such option {option}"
-        result = ''
-        result += option_to_sort(option, self.fl)
-        print(f"{result}")
-
-    def search_option(self, option, value):
-        assert isinstance(option, str), "Option should be str"
-        assert len(option) != 0, "Please enter an option"
-        assert option in ['life_time', 'price', 'length', 'color', 'fresh'], \
-            f"There is no such option {option}"
-        assert isinstance(value, int), "Value should be int"
-        assert value > 0, "Value must be positive"
-        result = ''
-        result += option_to_search(option, value, self.fl)
-        print(f"{result}")
-
-    def search_flower(self, el):
-        assert isinstance(el, str), "The name of a flower should be str"
-        assert len(el) != 0, "Please enter the name of a flower"
-        if el in [flower.name for flower in self.fl]:
-            print(f"{el} is in the bouquet")
+    def count(self, count_flowers_in_the_bouquet):
+        if Bouquet.count_flowers_in_the_bouquet % 2 == 0:
+            pass
         else:
-            print(f"{el} is not in the bouquet")
+            print(f"Не красиво дарить нечетное количество цветов: {count_flowers_in_the_bouquet}")
 
+    def price_of_bouquet(self,count_roses_in_the_bouquet, count_chamomile_in_the_bouquet, count_iris_in_the_bouquet, count_peon_in_the_bouquet):
+
+        price_rose = count_roses_in_the_bouquet * Roses.price
+        price_chamomile = count_chamomile_in_the_bouquet * Chamomile.price
+        price_iris = count_iris_in_the_bouquet * Iris.price
+        price_peon = count_peon_in_the_bouquet * Peony.price
+
+        sum = price_rose + price_chamomile + price_iris + price_peon
+        print(f'Цена букета равна {sum}')
+
+    def lifetime(self,count_roses_in_the_bouquet, count_chamomile_in_the_bouquet, count_iris_in_the_bouquet, count_peon_in_the_bouquet ):
+        pass
+
+
+bouquet = Bouquet()
+print(bouquet)
 
 
 
