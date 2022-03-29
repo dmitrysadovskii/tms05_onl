0e3bf4cf8f40bb0c87b424ac977e72235f7b6e92
Alesia Kapashilova <alesia.andreevna94@gmail.com>
2022-03-29 13:46:50 +0300

    fixed remarks

--- alesia_kapashilova/hm9/task_9_1.py
+++ alesia_kapashilova/hm9/task_9_1.py
@@ -1,3 +1,22 @@
+isbn_book = []
+
+
+def check_reserve(options, store, isbn):
+    result = ''
+    if len(store) == 0:
+        options += 1
+        store.append(isbn)
+        result += "You have"
+    else:
+        if isbn in store:
+            result += "should have a different ISBN, this one is used'"
+        else:
+            options += 1
+            store.append(isbn)
+            result += "You have"
+    return result
+
+
 class Book:
 
     def __init__(self, book, author, pages, isbn, reserved, taken):
@@ -12,22 +31,25 @@ class Book:
 class User:
 
     def __init__(self):
-        self.books = list()
+        self.books = []
 
     def book_to_take(self, us_book):
         self.books.append(us_book)
 
     def take_book(self):
         for us_book in self.books:
-            if us_book.taken not in self.books and us_book.reserved == 0:
-                print(f'You have taken {us_book.book}.')
-                us_book.taken += 1
-            elif us_book.taken == 0 and us_book.reserved != 0:
+            if us_book.taken == 0 in self.books and us_book.reserved != 0:
+                print(f'{us_book.book} has already reserved. '
+                      f'Please choose another book to take')
+            elif us_book.taken != 0:
                 print(f'{us_book.book} has already reserved. '
                       f'Please choose another book to take.')
-            elif us_book.taken != 0:
-                print(f'{us_book.book} has already taken. '
-                      f'Please choose another book to take.')
+            else:
+                result = check_reserve(us_book.taken, isbn_book, us_book.isbn)
+                if result == "You have":
+                    print(f'{result} taken {us_book.book}')
+                else:
+                    print(f"{us_book.book} {result}")
 
     def reserve_book(self):
         for us_book in self.books:
@@ -40,19 +62,17 @@ class User:
             elif us_book.reserved != 0:
                 print(f'{us_book.book} has already reserved. '
                       f'Please choose another book to reserve.')
+            else:
+                result = check_reserve(us_book.reserved, isbn_book,
+                                       us_book.isbn)
+                if result == 'You have':
+                    print(f'{result} reserved {us_book.book}')
+                else:
+                    print(f"{us_book.book} {result}")
 
     def return_book(self):
         for us_book in self.books:
             if us_book.taken == 1:
                 us_book.taken = 0
+                isbn_book.remove(us_book.isbn)
                 print(f'Thank you for bringing back {us_book.book}')
-
-
-book1 = Book('11/22/63', 'Стивен Кинг', 800, 0 - 586 - 63795 - 5, 0, 0)
-book2 = Book('Тьма между нами', 'Джон Марс', 320, 1 - 55404 - 295 - 7, 0, 0)
-User1 = User()
-User1.book_to_take(book1)
-User1.take_book()
-User2 = User()
-User2.book_to_take(book1)
-User2.reserve_book()
